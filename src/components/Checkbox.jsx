8b147d1f35306a5129358bd40ef89b90b3ffbf4f
Marc C <mcar176@gmail.com>
2023-04-27 09:23:26 +0200

    Forgot to delete deprecated code

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,13 +1,11 @@
-import ToggleButton from "./ToggleButton";
-
 const Checkbox = ({ label, checked, handleChange }) => {
   return (
     <div className="checkbox">
-      <label htmlFor={label}>
+      <label>
         <input
           id={label}
           type="checkbox"
-          checked={checked} // OK, mais dans l'inspector montre value
+          checked={checked}
           onChange={() => handleChange(label)}
         />
         <div className="toggle" />
--- src/components/ToggleButton.jsx
+++
@@ -1,9 +0,0 @@
-const ToggleButton = ({ checked }) => {
-  return (
-    <div className="toggle-button" data-checked={checked}>
-      <div></div>
-    </div>
-  );
-};
-
-export default ToggleButton;
